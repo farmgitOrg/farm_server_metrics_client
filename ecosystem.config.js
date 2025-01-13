e66a76b483f58c806e641d08420b30c110d65ba6
ylteng <ylteng@outlook.com>
2025-01-13 14:31:30 -0500

    update
    Signed-off-by: ylteng <ylteng@outlook.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
             env: {
                 HTTP_URL: "https://xxx",
                 CLIENT_ID: "test",
-                DISK_DEVICES: "/,/data", // df -l, device or mount point, split by ","; "" for all. eg. "/dev/nvme4n1p2,/dev/nvme4n1p1", or "/,/data"
-                NET_DEVICES: "bond",        // ifconfig,split by ","; "" for all. eg. ["bond"]
+                DISK_DEVICES: "/,/data", // df -l, device or mount point, split by ",",  "" for all. eg. "/dev/nvme4n1p2,/dev/nvme4n1p1", or "/,/data"
+                NET_DEVICES: "bond",     // ifconfig, split by ",", "" for all. eg. "bond"
                 SAMPLE_INTERVAL: 10,    // minutes
             }
         }
